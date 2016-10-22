64c62a89832cbcf11e05e767f18a2acb62f806e2
amglobal99 <amglobal99@yahoo.com>
2016-10-22 17:04:09 -0400

    Segmented Control is working now

--- xTester/TwelfthViewController.swift
+++ xTester/TwelfthViewController.swift
@@ -14,6 +14,47 @@ import UIKit
 class TwelfthViewController: UIViewController {
     
     
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            NSLog("First selected")
+        //show popular view
+        case 1:
+            NSLog("Second selected")
+        //show history view
+        default:
+            break;
+        }
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
