e430042e3da6d3170261c513ff59f1925e4cd278
amglobal99 <amglobal99@yahoo.com>
2016-11-13 22:38:20 -0500

    cosmetic

---
+++ xTester/TwelfthA2DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TwelfthA2DetailViewController.swift
+//  xTester
+//
+//  Created by Dad on 11/10/16.
+//  Copyright © 2016 Natsys. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import UIKit
+
+class TwelfthA2DetailViewController: UIViewController, Utils{
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        showBanner(title: "Banner Page", subtitle: "Welcome to Banner Page \n Plesase try later", image: nil, bkColor: UIColor.green)
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}  // end class
---
+++ xTester/TwelfthA2ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TwelfthA2ViewController.swift
+//  xTester
+//
+//  Created by Dad on 11/10/16.
+//  Copyright © 2016 Natsys. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class TwelfthA2ViewController: UICollectionViewController, Utils {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+       
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}  // end class
