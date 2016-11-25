e3e111689130abea4ec01ee9da1d2aebe9bfbfd6
amglobal99 <amglobal99@yahoo.com>
2016-11-25 16:31:34 -0500

    added code to TwelfthA2CollectionView3Cell

--- xTester/TwelfthA2CollectionView3Cell.swift
+++ xTester/TwelfthA2CollectionView3Cell.swift
@@ -11,12 +11,80 @@ import Foundation
 import UIKit
 
 
+protocol TwelfthA2CollectionView3CellDelegate
+{
+    //func deletePublisher(_ publisher: Publisher)
+}
+
+
 
 
 class TwelfthA2CollectionView3Cell: UICollectionViewCell {
     
     
     
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var photoIDLabel: UILabel!
+    @IBOutlet weak var photoServerLabel: UILabel!
+    
+    
+    
+    @IBAction func buyPhotoItem(_ sender: AnyObject) {
+        
+        let optionMenu = UIAlertController(title: nil, message: "Purchase Confirmaton Required ", preferredStyle: .actionSheet)
+        
+        let deleteAction = UIAlertAction(title: "Buy", style: .default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            print("Purchase will be made")
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+            print("Cancelled")
+        })
+        
+        optionMenu.addAction(deleteAction)
+        optionMenu.addAction(cancelAction)
+        
+        self.parentViewController?.present(optionMenu, animated: true, completion: {
+            print("completed presenting alert.....")
+            }
+        )
+        
+    } // end action
+    
+    
+    
+    func updateWithImage(_ image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner .stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner .startAnimating()
+            imageView.image = nil
+        }
+    }  //end method
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        updateWithImage(nil )
+    }
+    
+    
+    
+    
+    // called when cell is being reused
+    override func prepareForReuse() {
+        //print("             super.prepareforReuse")
+        super.prepareForReuse()
+        updateWithImage(nil )
+    }
+    
+    
+
     
     
     
