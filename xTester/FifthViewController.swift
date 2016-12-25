afad8c0a2394638b49c94f1548de7c02ef823aa2
amglobal99 <amglobal99@yahoo.com>
2016-12-25 16:26:14 -0500

    Enum references updated in FifthVC

--- xTester/FifthViewController.swift
+++ xTester/FifthViewController.swift
@@ -16,46 +16,16 @@ import Alamofire
 class FifthViewController: UIViewController, JsonConvertible, Utils {
 
     var jsonResultObject:JSON?
-    
-    //  Enable one of these to test
-    //let baseURLString  = Constants.Configuration.jsonTestUrl.flickr.rawValue
-    //let baseURLString  = Constants.Configuration.jsonTestUrl.bikeNYC.rawValue
-    //let baseURLString  = Constants.Configuration.jsonTestUrl.google.rawValue
-    //let baseURLString  = Constants.Configuration.jsonTestUrl.gitHub.rawValue
+    let testSiteName = "BIKENYC"  // change to "FLICKR" or "GITHUB" if needed
 
   
-  
-  
-  
-  
-  // UNCOMMent appropriate section as needed
-  
-  
-  /*
-  let apiKey = Constants.FlickrApi.apiKey
-  let method = Constants.FlickrApi.method
-  let key  = Constants.FlickrApi.key
-  let rootPath = Constants.FlickrApi.rootPath
-  let dataKey = Constants.FlickrApi.dataKey
-  let params = Constants.FlickrApi.params
-  */
-  
-  
-  
-  
-  let apiKey = Constants.BikeNycApi.apiKey
-  let method = Constants.BikeNycApi.method
-  let key  = Constants.BikeNycApi.key
-  let rootPath = Constants.BikeNycApi.rootPath
-  let dataKey = Constants.BikeNycApi.dataKey
-  let params = Constants.BikeNycApi.params
-  
- 
-  
-  
-    
     @IBAction func getKeyArray(_ sender: AnyObject) {
-        
+      
+            // Get access to test site details for Row #2
+            guard let testSite = Constants.Configuration.TestSite(rawValue: testSiteName) else {
+              return
+            }
+      
            //print("***** getKeyArray... starting *******")
             guard let jsonObj = self.jsonResultObject  else {
                 print("getKeyAray: Objcect is nil.")
@@ -63,22 +33,22 @@ class FifthViewController: UIViewController, JsonConvertible, Utils {
             }
         
             // get Section titles array ( key array)
-            guard let photoKeyArray =  getSectionTitlesArray(from: jsonObj, key: key)   else {
+            guard let photoKeyArray =  getSectionTitlesArray(from: jsonObj, key: testSite.key)   else {
                 print("@IBAction getKeyArray: photoKeyArray is nil.")
                 return
             }
         
-            print("+++++++++++++++++  @IBAction getKeyArray: Section Titles Array ---> key: \(key) +++++++++++++++++++++++")
+            print("+++++++++++++++++  @IBAction getKeyArray: Section Titles Array ---> key: \(testSite.key) +++++++++++++++++++++++")
             print(photoKeyArray)
             print("+++++++++++++++++  end @IBAction Section Titles +++++++++++++++++++++")
         
             // get Dictionary
-            guard let photoItemsDictionary = getDictionary(from: jsonObj,  for: key, keyArray: photoKeyArray, dataKey: dataKey)  else {
+            guard let photoItemsDictionary = getDictionary(from: jsonObj,  for: testSite.key, keyArray: photoKeyArray, dataKey: testSite.dataKey)  else {
                 print("@IBAction getKeyArray: PhotoItemsDictionary is nil.")
                 return
             }
             
-            print("+++++++++++++++++  @IBAction Dictonary: Key: \(key)  DataKey: \(dataKey) +++++++++++++++++++++++")
+            print("+++++++++++++++++  @IBAction Dictonary: Key: \(testSite.key)  DataKey: \(testSite.dataKey) +++++++++++++++++++++++")
             print(photoItemsDictionary)
             print("+++++++++++++++++  end @IBAction Dictionary +++++++++++++++++++++")
         
@@ -97,24 +67,23 @@ class FifthViewController: UIViewController, JsonConvertible, Utils {
                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
                let jsonObj = JSON(data: data)
                if jsonObj != JSON.null {
-               print("jsonData:\(jsonObj)")
+                   print("jsonData:\(jsonObj)")
+                    
+                   let arr:[String] = [""]
+                   print(arr.count)
+                   
+                   print("++++++++++++++++++++++ filter+++++++++")
+                   let prodObj = jsonObj["results"][0]["products"]
+                   print(prodObj)
+                   print("Length is : \(prodObj.count)  ")
+                  
+                   print("+++++++++++++++++++++++++++++++++++++")
+                   let salePrice = prodObj[0]["prices"]["sale"]
+                   print(salePrice)
+                   
+                   let imageType = prodObj[0]["images"][1]["type"]
+                   print(imageType)
                 
-               let arr:[String] = [""]
-               print(arr.count)
-               
-               print("++++++++++++++++++++++ filter+++++++++")
-               let prodObj = jsonObj["results"][0]["products"]
-               print(prodObj)
-               print("Length is : \(prodObj.count)  ")
-              
-               print("+++++++++++++++++++++++++++++++++++++")
-               let salePrice = prodObj[0]["prices"]["sale"]
-               print(salePrice)
-               
-               let imageType = prodObj[0]["images"][1]["type"]
-               print(imageType)
-               
-               
                } else {
                print("Could not get json from file, make sure that file contains valid json.")
                }
@@ -135,51 +104,28 @@ class FifthViewController: UIViewController, JsonConvertible, Utils {
   
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let testSite = Constants.Configuration.TestSite(rawValue: "BIKENYC") else {
+          return
+        }
       
-      
-      
-      guard let testSite = Constants.Configuration.TestSite(rawValue: "BIKENYC") else {
-        return
-      }
-      
-      
-
-      
-      
-      
-            // ********  Completion Handler *************
-            let completionHandler: (Result<JSON>) -> Void  =
-                { [weak self] result in
-                    guard let strongSelf = self else {
-                      return
-                    }
-                    strongSelf.jsonResultObject = result.value!   // set the value for local variable
-                    print(" ++++++++ Fifth VC: viewDidLoad: JSON Object +++++++++++ ")
-                     print(strongSelf.jsonResultObject)
-                    print(" ++++++++ Items List end from viewDidLoad  +++++++++++ ")
-                } // end closure
-      
-        
-        // Create a Async (Alamofire) request to get SwiftyJSON data
-      /*
-      guard let url = getSiteURL(baseURLString: baseURLString, method: method , parameters: params, apiKey: apiKey) else {
-        return
-      }
-      
-        getJSONObject(for: url, rootPath: rootPath,   completionHandler: completionHandler)  // get a SwiftyJSON object
+          // ********  Completion Handler *************
+          let completionHandler: (Result<JSON>) -> Void  =
+              { [weak self] result in
+                  guard let strongSelf = self else {
+                    return
+                  }
+                  strongSelf.jsonResultObject = result.value!   // set the value for local variable
+                  print(" ++++++++ Fifth VC: viewDidLoad: JSON Object +++++++++++ ")
+                   print(strongSelf.jsonResultObject)
+                  print(" ++++++++ Items List end from viewDidLoad  +++++++++++ ")
+              } // end closure
     
-  */
-      
       guard let url = getSiteURL(baseURLString: testSite.urlString, method: testSite.method , parameters: testSite.params, apiKey: testSite.apiKey) else {
         return
       }
-      
       getJSONObject(for: url, rootPath: testSite.rootPath,   completionHandler: completionHandler)  // get a SwiftyJSON object
       
-      
-  
-  
-  }  // end viewDidLoad
+    }  // end viewDidLoad
     
     
         
--- xTester/GlobalConstants.swift
+++ xTester/GlobalConstants.swift
@@ -11,8 +11,7 @@ import Alamofire
 
 
 
-    
-    typealias ClosureJSON<T> = (T) -> Void
+typealias ClosureJSON<T> = (T) -> Void
 
 
 
@@ -36,48 +35,9 @@ struct Constants {
     
     
     
-    
-    
-    struct FlickrApi {
-      
-      // Use This section for Flickr API
-      static let apiKey = "a6d819499131071f158fd740860a5a88"
-      static let method = "flickr.photos.getRecent"
-      static let params = ["extras":"url_h,date_taken"]
-      static let rootPath = ["photos","photo"]
-      static let key = "datetakenunknown"
-      static let dataKey = "id"
-      
-      enum Method: String {
-        case RecentPhotos = "flickr.photos.getRecent"
-      }
-
-    }  // end struct
-    
-  
-  
-  
-  
-  struct BikeNycApi {
-    
-    // Use this for bikeNYC API
-    static let apiKey:String? = nil
-    static let method:String? = nil
-    static let params:[String:String]? = nil
-    static let rootPath:[String]? = ["stationBeanList"]    // BikeNYC
-    static let key = "id"
-    static let dataKey = "statusValue"
+  struct flickrApi {
     
   }
-  
-  
-  
-  struct GistApi  {
-    static let baseUrl = "https://github.com"
-  }
-  
-  
-
   
   
     
@@ -86,91 +46,6 @@ struct Constants {
         static let companyName = "Natsys International"
         static let companyAddress = "1808 Mountain Lake Dr GA 30339"
       
-      /*
-      struct SiteParamaters {
-        let apiKey: String
-        let method: String
-        let params: [String:String]
-        let rootPath: [String]
-        let key: String
-        let dateKey: String
-      }
-      
-      
-      enum testWebSite {
-        case Flickr
-        case BikeNyc
-        case Gist
-        case Google
-      }
-      
-      */
-      
-      
-      
-      
-      
-      
-      
-      
-      
-        // Use This section for Flickr API
-         static let apiKey = "a6d819499131071f158fd740860a5a88"
-         static let method = "flickr.photos.getRecent"
-         static let params = ["extras":"url_h,date_taken"]
-         static let rootPath = ["photos","photo"]
-         static let key = "datetakenunknown"
-         static let dataKey = "id"
-        
-        
-        
-        
-         enum Method: String {
-            case RecentPhotos = "flickr.photos.getRecent"
-         }
-        
-      
- 
-        
-        /*
-        // Use this for Github API
-        static let apiKey:String? = nil
-        static let method:String? = nil
-        static let params:[String:String]? = nil
-        static let rootPath:[String]? = nil
-        static let key = "id"
-        static let dataKey = "clone_url"
-        */
-        
-      
-      
-      /*
-      
-               // Use this for bikeNYC API
-        static let apiKey:String? = nil
-        static let method:String? = nil
-        static let params:[String:String]? = nil
-        static let rootPath:[String]? = ["stationBeanList"]    // BikeNYC
-        static let key = "id"
-        static let dataKey = "statusValue"
-        
-        */
-      
-      
-      
-      
-
-        /*
-        // Use this for Google API
-        static let apiKey:String? = nil
-        static let method:String? = nil
-        static let params:[String:String]? = nil
-        static let rootPath:[String]? = ["responseData","feed","entries"]   // Google API
-        static let key = "author"
-        static let dataKey = "title"
-        
-        */
-        
       
       
       
@@ -198,7 +73,6 @@ struct Constants {
         case bikenyc
         case google
         case typicode
-        
         
         
         var urlString:String {
@@ -308,36 +182,6 @@ struct Constants {
         
       
       } // end enum
-      
-      
-
-
-      
-        /*
-        
-        // Enum store sthe various URLs we use for testing
-        enum jsonTestUrl:String {
-            // Flickr API: root is ["photos","photo"]
-            case flickr = "https://api.flickr.com/services/rest"
-            
-            // bikeNYC: root is  ["stationBeanList"]
-            case bikeNYC = "http://citibikenyc.com/stations/json"
-            
-            /// Google API: root is  ["responseData","feed","entries"]
-            case google = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=8&q=http%3A%2F%2Fnews.google.com%2Fnews%3Foutput%3Drss"
-            
-            // jsonplaceholder.typicode.com:   root is
-            case jsonPlaceholder1 = "https://jsonplaceholder.typicode.com/todos/1"
-            case jsonPlaceholder2 = "https://jsonplaceholder.typicode.com/todos"
-            
-            // GitHub: root is [] .... objects start at root
-            case gitHub = "https://api.github.com/users/amglobal99/repos"
-        }
-      
-      
-      */
-      
-      
       
       
       
