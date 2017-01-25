0db18ebeea02ec602a70baa6876ad97b249c9acc
Jack Patil <amglobal99@yahoo.com>
2017-01-25 13:14:14 -0500

    Added new copy of Nonthphoto

---
+++ xTester/NinthPhoto.swift
@@ -0,0 +1,50 @@
+
+//
+//  NinthPhoto.swift
+//  xTester
+//
+//  Created by Dad on 11/4/16.
+//  Copyright © 2016 Natsys. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// CLASS MODELS THE PHOTO OBJECT
+
+class NinthPhoto {
+  
+  let title: String
+  let remoteURL: URL
+  let photoID: String
+  let dateTaken: Date
+  var image: UIImage?
+  let datetakenUnknown:String
+  
+  
+  init(title: String, photoID: String, remoteURL: URL, dateTaken: Date, datetakenUnknown:String) {
+    self.title = title
+    self.photoID = photoID
+    self.remoteURL = remoteURL
+    self.dateTaken = dateTaken
+    //self.image = image
+    self.datetakenUnknown = datetakenUnknown
+  }
+  
+}  //end class
+
+
+
+
+
+
+extension NinthPhoto: Equatable {}
+
+
+func == (lhs:NinthPhoto, rhs: NinthPhoto) -> Bool  {
+  return lhs.photoID == rhs.photoID
+}
+
+
+
+
